754793af6bc68975b23ba60c28f08c3e794b438d
D4meror <benjaminsujemezki06@gmail.com>
2024-12-02 23:53:02 -0300

    Esta es la base del proyecto

---
+++ Votos/Votos/Clases/Datos/Conexion.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Votos.Clases.Datos
+{
+    internal class Conexion
+    {
+        private static string cadena = "Provider=Microsoft.ACE.OLEDB.12.0; Data source=|DataDirectory|Database.accdb";
+        private static OleDbConnection conexion = new OleDbConnection(cadena);
+        public static void AbrirConexion()
+        {
+            try
+            {
+                conexion.Open();
+            }
+            catch (Exception ex)
+            {
+                conexion.Close();
+                conexion.Open();
+            }
+        }
+
+        public static void CerrarConexion() { conexion.Close(); }
+
+        public static OleDbConnection GetConexion() { return conexion; }
+    }
+}
